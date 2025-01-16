625f8cbccbe43267d2b84f03e704cafff5e951cf
EgorHomePCkld <egorsid777@icloud.com>
2025-01-16 03:16:26 +0200

    FinishedResizing---NoBordersNoChangingNoCollide

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import pygame, os, sys
 
 
 pygame.init()
-pygame.display.set_caption('ChaseBlueSquare')
-size = width, height = 800, 800
-screen = pygame.display.set_mode(size)
+pygame.display.set_caption('Walking')
+size = width, height = 1400, 800
+screen = pygame.display.set_mode(size, pygame.RESIZABLE)
 
 
 def load_image(name, colorkey=None):
@@ -23,110 +23,220 @@ def load_image(name, colorkey=None):
     return image
 
 
-class MainChar(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        super().__init__(MCGroup)
-        self.a = 20
-        self.image = pygame.Surface((self.a, self.a),
-                                    pygame.SRCALPHA, 32)
-        pygame.draw.rect(self.image, 'blue',
-                         (0, 0, self.a, self.a))
-        self.rect = pygame.Rect(x, y, self.a, self.a)
+"""
+def load_level(filename):
+    filename = "data/game/" + filename
+    # читаем уровень, убирая символы перевода строки
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
 
-    def update(self, vx, vy):
-        if abs(vx) == abs(vy) == 5:
-            vx = vx / (2 ** 0.5)
-            vy = vy / (2 ** 0.5)
-        self.rect = self.rect.move(vx, vy)
+    # и подсчитываем максимальную длину
+    max_width = max(map(len, level_map))
+
+    # дополняем каждую строку пустыми клетками ('.')
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
 
 
-class MCBullet(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        super().__init__(BulletsGroup)
-        self.a = 10
-        self.b = 50
-        self.image = pygame.Surface((self.a, self.b),
-                                    pygame.SRCALPHA, 32)
-        pygame.draw.rect(self.image, pygame.Color('red'),
-                         (0, 0, self.a, self.b))
-        self.rect = pygame.Rect(x, y, self.a, self.b)
+def generate_level(level):
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '#':
+                Tile('tree', x, y)
+            elif level[y][x] == '@':
+                new_player = Player(x, y)
+    # вернем игрока, а также размер поля в клетках
+    return new_player, x, y
+"""
 
 
-class Enemy(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        super().__init__(EnemiesGroup)
-        self.a = 10
-        self.b = 10
-        self.image = pygame.Surface((self.a, self.b),
-                                    pygame.SRCALPHA, 32)
-        pygame.draw.rect(self.image, pygame.Color('yellow'),
-                         (0, 0, self.a, self.b))
-        self.rect = pygame.Rect(x, y, self.a, self.b)
-        self.collide_MC = False
+def terminate():
+    pygame.quit()
+    sys.exit()
 
-    def update(self, x2, y2, norm_v=5 / (2 ** 0.5)):
-        if not self.collide_MC:
-            x1, y1 = self.rect.x, self.rect.y
 
-            perp_x = x2 - x1
-            perp_y = y2 - y1
+def start_screen():
+    intro_text = ["ЗАСТАВКА", "",
+                  "Правила игры",
+                  "Если в правилах несколько строк,",
+                  "приходится выводить их построчно"]
 
-            dist = (perp_x ** 2 + perp_y ** 2) ** 0.5
-            if dist != 0:
-                vx = (perp_x / dist) * norm_v
-                vy = (perp_y / dist) * norm_v
-                collided_enemies = pygame.sprite.spritecollide(self, EnemiesGroup, False)
-                if len(collided_enemies) > 1:
-                    vx += abs(collided_enemies[1].rect.x - x1 - 15 * norm_v)
-                    vy += abs(collided_enemies[1].rect.y - y1 - 25 * norm_v)
-                    self.rect = self.rect.move(vx, vy)
-                elif pygame.sprite.spritecollideany(self, MCGroup):
-                    self.collide_MC = True
-                else:
-                    self.rect = self.rect.move(vx, vy)
+    fon = pygame.transform.scale(load_image('fon.jpg'), (width, height))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 40)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('black'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 30
+        intro_rect.top = text_coord
+        intro_rect.x = 30
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return  # начинаем игру
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+def final_screen():
+    outro_text = ["Окочание",
+                  "Спасибо за тестирование игры.",
+                  "Покедава!"]
+    screen.fill('white')
+    fon = pygame.transform.scale(load_image('fon2.jpg'), (width, height))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 40)
+    text_coord = 50
+    for line in outro_text:
+        string_rendered = font.render(line, 1, pygame.Color('black'))
+        outro_rect = string_rendered.get_rect()
+        text_coord += 30
+        outro_rect.top = text_coord
+        outro_rect.x = 30
+        text_coord += outro_rect.height
+        screen.blit(string_rendered, outro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        if tile_type == 'tree':
+            self.tile_width, self.tile_height = 100, 100
+        self.cur_img = tile_images[tile_type]
+        self.image = pygame.transform.scale(self.cur_img,
+                                            (width // (width // self.tile_width),
+                                             height // (height // self.tile_height)))
+        self.rect = self.image.get_rect().move(
+            pos_x, pos_y)
+
+    def resize(self, SW, SH):
+        global width, height
+        new_W, new_H = self.tile_width * (SW / width), self.tile_height * (SH / height)
+        self.image = pygame.transform.scale(self.cur_img,
+                                            (new_W, new_H))
+        self.tile_width, self.tile_height = new_W, new_H
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(player_group, all_sprites)
+        self.MC_img = player_image
+        self.image = pygame.transform.scale(self.MC_img, (width // (width // MC_width), height // (height // MC_height)))
+        self.rect = self.image.get_rect().move(
+             pos_x + 15, pos_y + 5)
+
+    def resize(self, SW, SH):
+        global MC_width, MC_height, width, height
+        new_W, new_H = MC_width * (SW / width), MC_height * (SH / height)
+        self.image = pygame.transform.scale(self.MC_img,
+                                            (new_W, new_H))
+        MC_width, MC_height = new_W, new_H
+
+
+class Camera:
+    def __init__(self):
+        self.dx = 0
+        self.dy = 0
+
+    def apply(self, obj):
+        obj.rect.x += self.dx
+        obj.rect.y += self.dy
+
+    def update(self, target):
+        self.dx = 0 #-(target.rect.x + target.rect.w // 2 - width // 2) - tile_width
+        self.dy = 0 #-(target.rect.y + target.rect.h // 2 - height // 2) - tile_height
+
+
+def level1(screen):
+    global background, width, height
+    resized_flag = False
+    camera = Camera()
+    tree_ab_x, tree_ab_y = width // int(1.3 * tree_n_w), height // int(0.7 * tree_n_h)
+    for x in range(0, width, tree_ab_x):
+        for y in range(0, height, tree_ab_y):
+            if x == 0 or y == 0:
+                Tile('tree', x, y)
+
+    while True:
+        screen.fill('black')
+        screen.blit(background, (0, 0))
+        keys = pygame.key.get_pressed()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_q:
+                return
+            if keys[pygame.K_a]:
+                MainCharacter.rect.x -= step
+            if keys[pygame.K_d]:
+                MainCharacter.rect.x += step
+            if keys[pygame.K_w]:
+                MainCharacter.rect.y -= step
+            if keys[pygame.K_s]:
+                MainCharacter.rect.y += step
+            if event.type == pygame.VIDEORESIZE:
+                resized_flag = True
+                new_SW, new_SH = event.w, event.h
+                background = pygame.transform.scale(background, (new_SW, new_SH))
+
+        camera.update(MainCharacter)
+        for sprite in all_sprites:
+            if resized_flag:
+                sprite.resize(new_SW, new_SH)
+            camera.apply(sprite)
+        if resized_flag:
+            width, height = new_SW, new_SH
+        resized_flag = False
+        tiles_group.draw(screen)
+        player_group.draw(screen)
+
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+all_sprites = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+
+tile_images = {
+    'tree': load_image(r'game\tree.png')
+}
+tree_n_w, tree_n_h = 20, 16
+background = pygame.transform.scale(load_image(r'game\background1.jpg'), (width, height))
+
+player_image = load_image('game\maincharacter.png')
+MC_width, MC_height = 50, 70
+MainCharacter = Player(width // 2, height // 2)
+
+clock = pygame.time.Clock()
+FPS = 60
+step = 10
+
+"""
+player, level_x, level_y = generate_level(load_level('lvl1.txt'))
+"""
 
 
 if __name__ == '__main__':
-    MCGroup = pygame.sprite.Group()
-    character = MainChar(50, 400)
-    MCGroup.add(character)
-    BulletsGroup = pygame.sprite.Group()
-    EnemiesGroup = pygame.sprite.Group()
-    for x in range(100, 780, 140):
-        for y in range(60, 760, 140):
-            EnemiesGroup.add(Enemy(x, y))
-    running = True
-    FPS = 60
-    clock = pygame.time.Clock()
-
-    while running:
-        vx, vy = 0, 0
-        screen.fill('black')
-
-        for event in pygame.event.get():
-            keys = pygame.key.get_pressed()
-            if event.type == pygame.QUIT:
-                running = False
-            if keys[pygame.K_a]:
-                    vx = -10
-            if keys[pygame.K_d]:
-                    vx = 10
-            if keys[pygame.K_w]:
-                    vy = -10
-            if keys[pygame.K_s]:
-                    vy = 10
-
-        chX, chY = character.rect.x, character.rect.y
-        MCGroup.update(vx=vx, vy=vy)
-        MCGroup.draw(screen)
-
-        BulletsGroup.draw(screen)
-
-        EnemiesGroup.update(chX, chY)
-        EnemiesGroup.draw(screen)
-
-
-        clock.tick(FPS)
-        pygame.display.flip()
-
-    pygame.quit()
+    start_screen()
+    level1(screen)
+    final_screen()
+    terminate()
